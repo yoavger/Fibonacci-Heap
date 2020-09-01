a0c0ebee240ff2a264242ce325deb77f4c4bc212
yoavger <59320825+yoavger@users.noreply.github.com>
2020-09-01 14:16:21 +0300

    Update FibonacciHeap.java

--- FibonacciHeap.java
+++ FibonacciHeap.java
@@ -33,7 +33,7 @@ public class FibonacciHeap
     * The method returns true if and only if the heap
     * is empty.
     *   
-    * Complexity --> )(1)
+    * Complexity --> O(1)
     */
     public boolean isEmpty()
     {
